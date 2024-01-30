48cf397f84481537f20b5d9503c6a29434016522
Thomas MICHEL-WEIL <tmichel-@paul-f3Br8s2.clusters.42paris.fr>
2024-01-30 16:51:21 +0100

    ex09

---
+++ 00/starting/ex09/ft_package/README.md
@@ -0,0 +1,1 @@
+This is an introduction to package create using simple setups.
--- 00/starting/ex09/ft_package/install.sh
+++ 00/starting/ex09/ft_package/install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# Générer les fichiers de distribution
 python setup.py sdist bdist_wheel
 
-# Installer le package localement
-pip install dist/ft_package-0.0.1.tar.gz
-# ou
-# pip install dist/ft_package-0.0.1-py3-none-any.whl
+# pip install dist/ft_package-0.0.1.tar.gz
+pip install dist/ft_package-0.0.1-py3-none-any.whl
