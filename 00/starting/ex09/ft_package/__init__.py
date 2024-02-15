5bab3eeebc0c92b717a08b387590c2996717e176
Thomas MICHEL-WEIL <tmichel-@bess-f1r1s3.clusters.42paris.fr>
2024-02-15 11:58:35 +0100

    structure modif

--- 00/starting/ex09/ft_package/__init__.py
+++
@@ -1,6 +0,0 @@
-def count_in_list(lst: list, to_count) -> int:
-    count = 0
-    for item in lst:
-        if item == to_count:
-            count += 1
-    return (count)
--- 00/starting/ex09/ft_package/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python setup.py sdist bdist_wheel
-
-# pip install dist/ft_package-0.0.1.tar.gz
-pip install dist/ft_package-0.0.1-py3-none-any.whl
